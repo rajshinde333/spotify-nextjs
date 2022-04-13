3a9eb480b437efd9fe93bca771ffe4eb170a8c08
rajshinde333 <rajshinde333rj@gmail.com>
2022-04-13 11:48:40 +0530

    first commit 13Mar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+//?Recoil JS for State Management like REDUX
+import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
